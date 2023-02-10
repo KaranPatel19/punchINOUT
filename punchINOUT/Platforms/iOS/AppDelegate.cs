db207de1bb7b466845585a36f9e028b6b0673ad5
karanpatel <A00264306@mycambrian.ca>
2023-02-10 15:05:42 -0500

    Initial Commit

---
+++ punchINOUT/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace punchINOUT;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
